69a5ca1355850d793ba45b4703049f742b7185ea
FlorianBodart <bodart.florian@outlook.fr>
2022-04-10 18:46:47 +0200

    version 0.6.0

--- js/main.js
+++ js/main.js
@@ -4,11 +4,10 @@ if (window.scrollY) {
   window.scroll(0, 0);  // reset the scroll position to the top left of the document.
 }
 
-
-let contact = document.querySelector("#contact");
 let main = document.querySelector("#main");
 let burger_menu = document.querySelector("#burger_menu");
 let burger_menu_ul = document.querySelector("#burger_menu_ul");
+
 
 
 burger_menu.onclick = function () {
@@ -18,6 +17,16 @@ burger_menu.onclick = function () {
 main.onclick = function () {
   burger_menu_ul.style.display = 'none';
 }
-contact.onclick = function () {
-  burger_menu_ul.style.display = 'none';
-}
+
+
+
+// let light = document.querySelector(".switch");
+
+// light.onclick = function () {
+
+// }
+// if (light.Checked == true){
+//   Text.style.display = "block";
+// } else {
+//   Text.style.display = "none";
+// }
